98d9a3702166d69d093283a82c1fb8333db8b0b6
trident-kanda <tridentkandayuki@gmail.com>
2021-04-03 05:34:58 +0900

    slickscroll

--- components/Slick.tsx
+++ components/Slick.tsx
@@ -8,6 +8,8 @@ const Slick = () => {
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
   };
   return (
     <Slider {...setting} className="border-4 border-gray-200 rounded-md">
