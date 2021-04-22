d20baec3060a3f2fdf80905f1991524b201f050e
trident-kanda <tridentkandayuki@gmail.com>
2021-04-22 11:53:58 +0900

    document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,8 +1,16 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+import type { DocumentContext, DocumentInitialProps } from "next/document";
+
 class MyDocument extends Document {
-  render() {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    const initialProps = await Document.getInitialProps(ctx);
+    return initialProps;
+  }
+  render(): JSX.Element {
     return (
       <Html lang="ja">
         <Head />
