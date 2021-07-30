7d63e7352ce2cf3f1c496b60042e244d3abaab80
trident-kanda <tridentkandayuki@gmail.com>
2021-07-30 20:03:03 +0900

    none

--- components/ArrowLink.tsx
+++ components/ArrowLink.tsx
@@ -5,11 +5,33 @@ type props = {
   internal: boolean;
 };
 const ArrowLink = ({ link, word, internal }: props) => {
-  return (
-    <div className=" text-center mt-5">
-      {internal === true && (
-        <Link href={link}>
-          <a>
+  if (link === "") {
+    return (
+      <div className=" text-center mt-5">
+        <p className="font-extrabold inline text-lg cursor-not-allowed text-gray-500">
+          準備中👷
+        </p>
+      </div>
+    );
+  } else {
+    return (
+      <div className=" text-center mt-5">
+        {internal === true && (
+          <Link href={link}>
+            <a>
+              <div className=" inline link">
+                <p className="font-extrabold inline text-lg  cursor-pointer  ">
+                  {word}
+                </p>
+                <div className="inline-block">
+                  <div className="inline arrow_right"></div>
+                </div>
+              </div>
+            </a>
+          </Link>
+        )}
+        {internal === false && (
+          <a href={link} rel="nofollow" target="_blank">
             <div className=" inline link">
               <p className="font-extrabold inline text-lg  cursor-pointer  ">
                 {word}
@@ -19,22 +41,10 @@ const ArrowLink = ({ link, word, internal }: props) => {
               </div>
             </div>
           </a>
-        </Link>
-      )}
-      {internal === false && (
-        <a href={link} rel="nofollow" target="_blank">
-          <div className=" inline link">
-            <p className="font-extrabold inline text-lg  cursor-pointer  ">
-              {word}
-            </p>
-            <div className="inline-block">
-              <div className="inline arrow_right"></div>
-            </div>
-          </div>
-        </a>
-      )}
-    </div>
-  );
+        )}
+      </div>
+    );
+  }
 };
 
 export default ArrowLink;
